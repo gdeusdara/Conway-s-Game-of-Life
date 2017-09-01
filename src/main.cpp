d1dd1083d0c0c3e754bb2d76f2c3bb4a67cb33a1
Guilherme Deusdara <guibanci@gmail.com>
2017-09-01 14:09:51 -0300

    testando o campo na main

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "celula.hpp"
+#include "campo.hpp"
+#include <string>
+
+using namespace std;
+
+int main(int argc, char ** argv) {
+	campo x;
+
+	x.getMatriz();
+
+
+	return 0;
+}
