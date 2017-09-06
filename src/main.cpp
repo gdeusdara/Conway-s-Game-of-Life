caf72042485b774536419d312cd2d804d66bb01d
Guilherme Deusdara <guibanci@gmail.com>
2017-09-06 09:46:55 -0300

    Melhoria nos nomes das variáveis

--- src/main.cpp
+++ src/main.cpp
@@ -7,64 +7,64 @@
 using namespace std;
 
 int main(int argc, char ** argv) {
-	campo x, y;
+	campo principal, complementar;
 	int linha;
 	int coluna;
 	int contagem;
 	int tempo = 0;
 
-	x.setGosperGliderGun();
+	principal.setGosperGliderGun();
 
-	 while(tempo < 100){
+	 while(tempo < 500){
 
-		x.getMatriz();
-		usleep(300000);
+		principal.getMatriz();
+		usleep(150000);
 
 		for (linha = 0; linha < 57; linha++)
 			for (coluna = 0; coluna < 220; coluna++){
 				contagem = 0;
 				if (linha - 1 >= 0){
-					if (x.getEstado(linha - 1, coluna) == 'E')
+					if (principal.getEstado(linha - 1, coluna) == 'E')
 						contagem++;
 					if (coluna - 1 >= 0)
-						if (x.getEstado(linha - 1, coluna - 1) == 'E')
+						if (principal.getEstado(linha - 1, coluna - 1) == 'E')
 							contagem++;
 					if (coluna + 1 < 220)
-						if (x.getEstado(linha - 1, coluna + 1) == 'E')
+						if (principal.getEstado(linha - 1, coluna + 1) == 'E')
 							contagem++;
 				}
 				if (coluna - 1 >= 0)
-					if(x.getEstado(linha, coluna - 1) == 'E')
+					if(principal.getEstado(linha, coluna - 1) == 'E')
 						contagem++;
 				if (coluna + 1 < 220)
-					if(x.getEstado(linha, coluna + 1) == 'E')
+					if(principal.getEstado(linha, coluna + 1) == 'E')
 						contagem++;
 				if(linha + 1 < 57){
-					if(x.getEstado(linha + 1, coluna) == 'E')
+					if(principal.getEstado(linha + 1, coluna) == 'E')
 						contagem++;
 					if(coluna - 1 >= 0)
-						if(x.getEstado(linha + 1, coluna - 1) == 'E')
+						if(principal.getEstado(linha + 1, coluna - 1) == 'E')
 							contagem++;
 					if(coluna + 1 < 220)
-						if(x.getEstado(linha +1, coluna + 1) == 'E')
+						if(principal.getEstado(linha +1, coluna + 1) == 'E')
 							contagem++;
 				}
 				
-				if(x.getEstado(linha, coluna) == '-'){
+				if(principal.getEstado(linha, coluna) == '-'){
 					if(contagem == 3){
-						y.setViver(linha, coluna);
+						complementar.setViver(linha, coluna);
 					}
 				}else{
 					if(contagem < 2 or contagem > 3){
-						y.setMorrer(linha, coluna);
+						complementar.setMorrer(linha, coluna);
 					}else{
-						y.setViver(linha,coluna);
+						complementar.setViver(linha,coluna);
 					}
 				}
 				
 			}
 
-		x = y;
+		principal = complementar;
 		tempo++;
 		
 	}
