9f5682d9cd9f0f173a52620243ce8ea3368f8686
Guilherme Deusdara <guibanci@gmail.com>
2017-09-15 16:38:27 -0300

    classe forma criada (nos termos do professor)

---
+++ inc/forma.hpp
@@ -0,0 +1,27 @@
+#ifndef FORMA_HPP
+#define FORMA_HPP
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class forma {
+private:
+	vector<int> coordenadas_x;
+	vector<int> coordenadas_y;
+
+public:
+	forma();
+	~forma();
+
+	int getQuantidadeCoordenadas();
+	int getCoordenada_x(int posicao);
+	int getCoordenada_y(int posicao);
+	void acrescentaCordenadas(int coordenada_x, int coordenada_y);
+
+};
+
+
+#endif
---
+++ src/forma.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include "forma.hpp"
+
+using namespace std;
+
+forma::forma(){};
+forma::~forma(){};
+
+int forma::getQuantidadeCoordenadas(){
+	return coordenadas_x.size();
+}
+int forma::getCoordenada_x(int posicao){
+	return coordenadas_x[posicao];
+}
+
+int forma::getCoordenada_y(int posicao){
+	return coordenadas_y[posicao];
+}
+
+void forma::acrescentaCordenadas(int coordenada_x, int coordenada_y){
+	coordenadas_x.push_back(coordenada_x);
+	coordenadas_y.push_back(coordenada_y);
+}
